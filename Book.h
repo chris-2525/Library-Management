d911060a838416cf731a97a3dbd3dbd365f10a91
chris-2525 <144377615+chris-2525@users.noreply.github.com>
2023-09-07 14:12:04 -0400

    Update Book.h

--- Book.h
+++ Book.h
@@ -1,16 +1,4 @@
-// Final Project Milestone 5
-// Date Module
-// File	Book.h
-// Version 1.0
-// Author	Christine Ang; Student ID#: 121559223
-// Revision History
-// -----------------------------------------------------------
-// Name               Date                 Reason
-// 
-// I have done all the coding by myself and only copied the code 
-// that my professor provided to complete my workshops and assignments
-// 
-/////////////////////////////////////////////////////////////////
+
 #ifndef SDDS_BOOK_H__
 #define SDDS_BOOK_H__
 #include <iostream>
