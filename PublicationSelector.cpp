e97d26b3f4972efc6442a8f2655112e0792b595f
chris-2525 <144377615+chris-2525@users.noreply.github.com>
2023-09-07 14:14:34 -0400

    Update PublicationSelector.cpp

--- PublicationSelector.cpp
+++ PublicationSelector.cpp
@@ -1,13 +1,4 @@
-// Final Project 
-// PublicationSelector
-// File PublicationSelector.h
-// Version 1.0
-// Author	Fardad Soleimanloo
-// Revision History
-// -----------------------------------------------------------
-// Name               Date                 Reason
-//
-/////////////////////////////////////////////////////////////////
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>
